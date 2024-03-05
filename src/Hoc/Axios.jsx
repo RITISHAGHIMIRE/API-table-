82da6f591d803571263e0df899a8fe10dd954145
Ritisha ghimire <107596067+RITISHAGHIMIRE@users.noreply.github.com>
2024-03-05 08:12:16 +0545

    Update Axios.jsx

--- src/Hoc/Axios.jsx
+++ src/Hoc/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 const axiosinstance=axios.create({
-    baseURL:'http://192.168.100.43:3000/'
-    // baseURL:"https://hubmainback.hubit.com.np/"
+    // baseURL:'http://192.168.100.43:3000/'
+    baseURL:"https://hubmainback.hubit.com.np/"
 })
 
 axiosinstance.defaults.headers.common["Authorization"]="Bearer "+localStorage.getItem("token")
